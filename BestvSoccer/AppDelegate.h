3eb1834cf6f9fbfa9d9247cd9510918ad78e52f6
Apple <Apple@AppledeiMac.local>
2014-06-16 09:08:51 +0800

    Initial Commit

---
+++ BestvSoccer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BestvSoccer
+//
+//  Created by Apple on 14-6-16.
+//  Copyright (c) 2014年 xxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
